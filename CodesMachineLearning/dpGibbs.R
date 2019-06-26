72a4b0ca9894875f62922ab7598a6072da3b7575
Babak Shahbaba <babaks@uci.edu>
2019-06-26 11:06:08 -0700

    Update dpGibbs.R

--- CodesMachineLearning/dpGibbs.R
+++ CodesMachineLearning/dpGibbs.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 
-# We generate data from two normal distributions: 20 samples with means 0 and 10 samples with 5; we fist the variance at 1
-set.seed(1)
+# We generate data from two normal distributions: 20 samples with means 0 and 10 samples with 5; we fix the variance at 1
+set.seed(123)
 x <- c(rnorm(20, 0, 1), rnorm(10, 5, 1))
 n = length(x)
 
@@ -11,9 +11,9 @@ nIter = 10000
 # We set gamma to 3. In genral, however, it's better to put a prior on gamma
 gamma <- 3
 
-# Baeline distribution G0 is assumed to be N(0, 10)
+# Baeline distribution G0 is assumed to be N(0, 100)
 m = 0
-tau2 <- 10
+tau2 <- 100
 
 # Here, we fist the variance at 1 so we are not sampling it. 
 sigma2 <- 1
@@ -25,7 +25,7 @@ theta <- matrix(0, n, nIter)
 for(j in 1:nIter){
   for(i in 1:n){
     # Probability (up to some constant) that the mean comes from G0
-    q0 <- gamma*dnorm(x[i], m, sigma2+tau2) 
+    q0 <- gamma*dnorm(x[i], m, sqrt(sigma2+tau2)) 
     
     # Probability that the mean for the ith observation is the same as the mean of the jth observation
     qj <- dnorm(x[i], theta[-i, j], 1) 
@@ -34,7 +34,7 @@ for(j in 1:nIter){
     p = c(q0, qj)/sum(c(q0, qj)) 
     
     # We sample a new theta from the posterior distribution given G0 and create a vector of possible means to choose from; the elements of this vector correspond to p
-    Thetas <- c( rnorm(1, (m/tau2+x[i]/sigma2)/(1/tau2+1/sigma2), 1/(1/tau2+1/sigma2) ), theta[-i, j]) 
+    Thetas <- c( rnorm(1, (m/tau2+x[i]/sigma2)/(1/tau2+1/sigma2), sqrt( 1/(1/tau2+1/sigma2) ) ), theta[-i, j]) 
     
     # we then sample a theta with probabilty p
     theta[i, j] <- sample(Thetas, 1, prob=p) 
@@ -43,7 +43,7 @@ for(j in 1:nIter){
 
 
 
-# We throguh away the first 3000 samples and take average using the remaining samples
+# We throw away the first 3000 samples and take average using the remaining samples
 plot(rowMeans(theta[, 3001:nIter]))
 
 
@@ -61,9 +61,9 @@ for(i in seq(5001, 10000, 500)){
 jpeg('../figures/dpUniDensity.jpeg')
 simData.df <- data.frame(x=Y[, 1], y = Y[, 2])
 origData.df <- data.frame(x = x, y=rep(0, n))
-p <- ggplot(simData.df, aes(x=x, group=y)) + geom_density() # +theme(legend.position="none")
-p <- p + geom_point(aes(x=x, y=y), size=4, cex=16, data=df)
-p
+p <- ggplot(simData.df, aes(x=x, group=y)) + geom_density() 
+p <- p + geom_point(aes(x=x, y=y), cex=2, data=origData.df)
+print(p)
 dev.off()
 
 
